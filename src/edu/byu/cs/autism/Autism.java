b1933e8a2f821e6df65c08219b55b338ae1aa5f5
Joseph Bills <roboiguana@gmail.com>
2018-11-06 19:03:37 -0700

    Added test stuff to conversation class

--- src/edu/byu/cs/autism/Autism.java
+++ src/edu/byu/cs/autism/Autism.java
@@ -23,17 +23,7 @@ import java.util.List;
 public class Autism extends JavaPlugin implements Listener {
 
 
-    List<Conversation> conversations;
 
-    public void testConversations(){
-        //should be called each tick, somehow
-        for(int i = conversations.size() - 1; i >= 0; i--){
-            conversations.get(i).update();
-            if(!conversations.get(i).active) {
-                conversations.remove(i);
-            }
-        }
-    }
 
     private final FriendMiniGameHistory friendMiniGameHistory = new FriendMiniGameHistory();
     RelationshipPH rph;
@@ -81,7 +71,7 @@ public class Autism extends JavaPlugin implements Listener {
             player.sendMessage( "Direction vector: " + EyeContact.direction(player.getEyeLocation().getPitch(), player.getEyeLocation().getYaw()).toString());
             player.sendMessage("Cosine Sim: " + EyeContact.cossime(EyeContact.eyeToeye(player,other),EyeContact.direction(player.getEyeLocation().getPitch(), player.getEyeLocation().getYaw())));
 
-            conversations.add(new Conversation(player, other));
+            Conversation.conversations.add(new Conversation(player, other));
 
             RelationshipLevel.initiateConversation(player.getUniqueId().toString(), other.getUniqueId().toString());
             rph.setActiveOther(player.getUniqueId().toString(), other.getUniqueId().toString());
@@ -130,7 +120,11 @@ public class Autism extends JavaPlugin implements Listener {
 
             Player player = getServer().getPlayer(sender.getName());
             sender.sendMessage("Center: " + player.getLocation().toString() + " Eye: " + player.getEyeLocation().toString());
+        }  else if ("testConversations".equalsIgnoreCase(command.getName())){
+
+            Conversation.handleCommand("testConversations");
         }
+
         return super.onCommand(sender, command, label, args);
     }
 }
--- src/edu/byu/cs/autism/Conversation.java
+++ src/edu/byu/cs/autism/Conversation.java
@@ -2,10 +2,29 @@ package edu.byu.cs.autism;
 
 import org.bukkit.entity.Player;
 
+import java.util.List;
+
 public class Conversation {
 
     Player one;
     Player two;
+
+    static  List<Conversation> conversations;
+
+    static public void testConversations(){
+        //should be called each tick, somehow
+        for(int i = conversations.size() - 1; i >= 0; i--){
+            conversations.get(i).update();
+            if(!conversations.get(i).active) {
+                conversations.remove(i);
+            }
+        }
+    }
+
+    //getConversation(Player one, Player two)
+   public static void handleCommand(String command){
+        testConversations();
+    }
 
     public Conversation(Player one, Player two) {
         this.one = one;
@@ -21,7 +40,7 @@ public class Conversation {
 
 
     void update(){
-        if(EyeContact.eyeContact(one,two)){ //later also check for distance
+        if(EyeContact.eyeContact(one,two)){ //later also check for distance, if speaking
             gracePoints = GRACE_SPAN;
         } else {
             gracePoints--;
@@ -29,8 +48,12 @@ public class Conversation {
 
         if(gracePoints <= 0){
             active = false;
+            one.sendMessage("Your conversation with " + two.getDisplayName() + " has ended.");
+            two.sendMessage( "Your conversation with " + one.getDisplayName() + " has ended.");
         }
 
     }
+
+
 
 }
